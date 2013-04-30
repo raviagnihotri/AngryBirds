bf95a62c6d8b833a4b88cc9ff035ff6854ea2aec
Andreas Baaserud <andreasbaaserud@github.com>
2013-04-30 13:42:01 +0200

    Jobber med fysikkmotor, problemer med Y-coordinater

--- FysikkMotor.cpp
+++ FysikkMotor.cpp
@@ -9,11 +9,11 @@ float sinSpeed;
 float cosSpeed;
 float timeY;
 float timeYHeight;
-float distanceYHeight;
+float distanceY_up;
 float totalHeight;
 float timeYDown;
 float totalTime;
-float totalDistance;
+float distanceX;
 float totalHeightHelper;
 float lengdeHelper;
 
@@ -53,16 +53,46 @@ void FysikkMotor::totalDistanceX(){
         cosSpeed = cos(getAngle()*PI/180) * getSpeed();
         timeY = (sinSpeed / (0.5*9.81));
         timeYHeight = timeY / 2;
-        distanceYHeight = (sinSpeed * timeYHeight) - (0.5*9.81 * (timeYHeight * timeYHeight));
-        totalHeight = height + distanceYHeight;
+        distanceY_up = (sinSpeed * timeYHeight) - (0.5*9.81 * (timeYHeight * timeYHeight));
+        totalHeight = height + distanceY_up;
         totalHeightHelper = lengdeHelper--;
         timeYDown = sqrt(totalHeight / (0.5 * 9.81));
         totalTime = timeYDown + timeYHeight;
-        totalDistance = cosSpeed * totalTime;
-
-        cout << "X: " << i << "Y: " << totalHeightHelper << endl;
+        distanceX = cosSpeed * totalTime;
     }
-        cout << "Total distanse: " << ceil(totalDistance) << " meter" << endl;
+
+//    int ceil_distanceX_Coord = ceil(distanceX_Coord);
+    int distanceX_Coord;
+    int distanceY_Coord = 0;
+    int ceil_cosSpeed = ceil(cosSpeed);
+    int ceil_sinSpeed = ceil(sinSpeed);
+    int ceil_totalTime = ceil(totalTime);
+    int ceil_distanceX = ceil(distanceX);
+    int ceil_distanceY = ceil(totalHeight);
+
+//    for(int i = 0; i < ceil_totalTime; i++){
+//        distanceX_Coord = ceil_cosSpeed * i;
+//        distanceY_Coord = tan(angle) * i;
+//        cout << "(" << distanceX_Coord << ", " << distanceY_Coord << ")" << endl;
+//    }
+
+    for(int i = 0; i < ceil_distanceX; i++){
+        distanceY_Coord = -tan(angle) * i;
+        cout << "(" << i << ", " << distanceY_Coord << ")" << endl;
+    }
+//    for(int i = 0; i <= ceil_distanceX; i++){
+//        cout << "X: " << i << endl;
+//    }
+
+//    for(int i = 0; i <= ceil_distanceY; i++){
+//        cout << "Y: " << i << endl;
+//    }
+        cout << "Distance Y, up: " << ceil(distanceY_up) << " meter" << endl;
+        cout << "Time Y, up " << ceil(timeYHeight) << " sec" << endl;
+        cout << "Distance Y, down: " << ceil(totalHeight) << " meter" << endl;
+        cout << "Time total, down: " << ceil(timeYDown) << " sec" << endl;
+        cout << "Distance X: " << ceil(distanceX) << " meter" << endl;
+        cout << "Time total: " << ceil(totalTime) << " sec" << endl;
 
 //    return ceil(totalDistance);
 }
