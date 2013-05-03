6e07229f0cb54ad529782378ee75c8a1ac476779
Ravi Ubuntu-VM <ravi@ravi-VirtualBox.(none)>
2013-05-03 11:51:36 +0200

    Pusset på grafikken, lagt inn grenseverdier for speed/angle

--- Game.cpp
+++ Game.cpp
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -15,10 +16,10 @@ float gravity;
 
 WINDOW * top;
 WINDOW * btm;
-int toprow;
+int toprow, topx, topy;
 
 vector<string> logo(14);
-vector<string> sun(5);
+vector<string> sun(7);
 
 void Game::initLogo()
 {
@@ -29,22 +30,52 @@ void Game::initLogo()
     logo[4] = "     \\ \\ \\/\\ \\ \\ \\`\\ \\ \\ \\/, \\ \\ \\\ \\  `\\ \\ \\  ";
     logo[5] = "      \\ \\_\\ \\_\\ \\_\\ \\_\\ \\____/\\ \\_\\ \\_\\ \\ \\_\\  ";
     logo[6] = "       \\/_/\\/_/\\/_/\\/_/\\/___/  \\/_/\\/_/  \\/_/ ";
-    logo[7] = "         ____    ______   ____    ____    ____      ";
-    logo[8] = "        /\\  _`\\ /\\__  _\\ /\\  _`\\ /\\  _`\\ /\\  _`\\   ";
-    logo[9] = "        \\ \\ \\L\\ \\/_\/\\ \\/ \\ \\ \\L\\ \\ \\ \\/\\ \\ \\,\\L\\_\\  ";
-    logo[10] = "         \\ \\  _ <' \\ \\ \\  \\ \\ ,  /\\ \\ \\ \\ \\/_\\__ \\  ";
-    logo[11] = "          \\ \\ \\L\\ \\ \\_\\ \\__\\ \\ \\\\ \\\\ \\ \\_\\ \\/\\ \\L\\ \\ ";
-    logo[12] = "           \\ \\____/ /\\_____\\\\ \\_\\ \\_\\ \\____/\\ `\\____\\ ";
-    logo[13] = "            \\/___/  \\/_____/ \\/_/\\/ /\\/___/  \\/_____/ ";
+    logo[7] = "          __  __  ____    ____    ____    ____         ";
+    logo[8] = "         /\\ \\/\\ \\/\\  _`\\ /\\  _`\\ /\\  _`\\ /\\  _`\\       ";
+    logo[9] = "         \\ \\ `\\\\ \\ \\ \\L\\_\\ \\ \\L\\ \\ \\ \\/\\ \\ \\,\\L\\_\\     ";
+    logo[10] = "          \\ \\ , ` \\ \\  _\\L\\ \\ ,  /\\ \\ \\ \\ \\/_\\__ \\     ";
+    logo[11] = "           \\ \\ \\`\\ \\ \\ \\L\\ \\ \\ \\\\ \\\\ \\ \\_\\ \\/\\ \\L\\ \\   ";
+    logo[12] = "            \\ \\_\\ \\_\\ \\____/\\ \\_\\ \\_\\ \\____/\\ `\\____\\  ";
+    logo[13] = "             \\/_/\\/_/\\/___/  \\/_/\\/ /\\/___/  \\/_____/  ";
 }
 
 void Game::initSun()
 {
-    sun[0] = "###########";
-    sun[1] = "##########";
-    sun[2] = "#########";
-    sun[3] = "######";
-    sun[4] = "###";
+    sun[0] = "############################";
+    sun[1] = "#########################";
+    sun[2] = "##################";
+    sun[3] = "##############";
+    sun[4] = "#########";
+    sun[5] = "####";
+    sun[6] = "#";
+}
+
+int Game::randomNumber(int s)
+{
+    return (rand()%s);
+}
+
+void Game::printScenery()
+{
+    wattron(top, A_BOLD | COLOR_PAIR(4));
+    //Printe ut sola...
+    for(int i = 0; i < sun.size(); i++)
+        mvwaddstr(top,i+1,1, sun[i].c_str());
+    wattroff(top, A_BOLD | COLOR_PAIR(4));
+
+    wattron(top, COLOR_PAIR(7));
+    //...og litt gress og blomster
+    for(int i = 1; i < topx-1; i++)
+        mvwaddstr(top,topy-2,i, "|");
+
+    for(int i = 1; i < 15; i++)
+    {
+        wattron(top, A_BOLD | COLOR_PAIR(8+(i%3)));
+        mvwaddstr(top,topy-3,randomNumber(topx-1), "*");
+    }
+
+
+    wattroff(top, COLOR_PAIR(4));
 }
 
 void Game::initScreen()
@@ -52,7 +83,7 @@ void Game::initScreen()
     // Start ncurses
     initscr();
 
-    int row, col, toprow, topcol, btmrow, btmcol;
+    int row, col, topcol, btmrow, btmcol;
 
     //Ikke tegn på skjermen ved brukerinput
     noecho();
@@ -72,6 +103,9 @@ void Game::initScreen()
     top = newwin(toprow, col, 0, 0);
     btm = newwin(btmrow, col, btmcol, 0);
 
+    topy = getmaxy(top);
+    topx = getmaxx(top);
+
     refresh();
 
     //Opprette noen fargepar
@@ -81,6 +115,10 @@ void Game::initScreen()
     init_pair(4, COLOR_YELLOW, COLOR_BLACK);
     init_pair(5, COLOR_MAGENTA, COLOR_WHITE);
     init_pair(6, COLOR_WHITE, COLOR_BLACK);
+    init_pair(7, COLOR_GREEN, COLOR_BLACK);
+    init_pair(8, COLOR_RED, COLOR_GREEN);
+    init_pair(9, COLOR_BLUE, COLOR_GREEN);
+    init_pair(10, COLOR_YELLOW, COLOR_GREEN);
 
 }
 
@@ -92,16 +130,12 @@ void Game::welcomeScr()
     box(top, 0, 0);
     box(btm, 0, 0);
 
-    wattron(top, A_BOLD | COLOR_PAIR(4));
-    //Printe ut sola
-    for(int i = 0; i < sun.size(); i++)
-        mvwaddstr(top,i+1,1, sun[i].c_str());
-    wattroff(top, COLOR_PAIR(4));
+    printScenery();
 
     wattron(top, A_BOLD | COLOR_PAIR(1));
-    //Printe ut logoen
+    //Printe ut logoen i midten
     for(int i = 0; i < logo.size(); i++)
-        mvwaddstr(top,i+7,10, logo[i].c_str());
+        mvwaddstr(top,i+9,(topx-55)/2, logo[i].c_str());
     wattroff(top, COLOR_PAIR(1));
 
 
@@ -138,7 +172,6 @@ void Game::userInput()
             while(true)
             {
                 stagePick = getch();
-//                int in2 = getch();
                 if(stagePick == 49)
                 {
                     startStage(1);
@@ -205,13 +238,15 @@ void Game::startStage(int in)
     wrefresh(top);
     wrefresh(btm);
 
+    //Printer ut fuglen før launch
     for(int i = 0; i < bird.size(); i++)
     {
-        mvwaddstr(top, 27+i, 1, bird[i].c_str());
+        mvwaddstr(top, ((topy-2)-bird.size())+i, 1, bird[i].c_str());
         mvwaddstr(top, 30, stage->getEnemyDistance()-5, "[");
         mvwaddstr(top, 30, stage->getEnemyDistance(), "X");
         mvwaddstr(top, 30, stage->getEnemyDistance()+5, "]");
     }
+    printScenery();
 
     mvwprintw(btm, 1, 1, "Speed: %d", speed);
     mvwprintw(btm, 2, 1, "Angle: %d", angle);
@@ -221,45 +256,47 @@ void Game::startStage(int in)
     while(true)
     {
         int in = getch();
-        if(in == KEY_UP)
+        if(in == KEY_UP) //Juster fart opp
         {
-            speed++;
+            if(speed < 200) speed++;
             mvwprintw(btm, 1, 1, "Speed: %d", speed);
             wrefresh(btm);
         }
-        else if(in == KEY_DOWN)
+        else if(in == KEY_DOWN) //...ned
         {
-            speed--;
+            if(speed > 1) speed--;
             mvwprintw(btm, 1, 1, "Speed: %d", speed);
             wrefresh(btm);
         }
-        else if(in == KEY_LEFT)
+        else if(in == KEY_LEFT) //Juster vinkel ned
         {
-            angle++;
+            if(angle < 90) angle++;
             mvwprintw(btm, 2, 1, "Angle: %d", angle);
             wrefresh(btm);
         }
-        else if(in == KEY_RIGHT)
+        else if(in == KEY_RIGHT) //...opp
         {
-            angle--;
+            if(angle > 1) angle--;
             mvwprintw(btm, 2, 1, "Angle: %d", angle);
             wrefresh(btm);
         }
-        else if(in == 97) //bokstaven a
+        else if(in == 97) //Lift-off!
         {
-            stage->setUserInput((float) speed, (float) angle, 1);
+            stage->setUserInput((float) speed, (float) angle, 1); //regner ut bane/trajectory
             wrefresh(top);
+            int atgrass = ((topy-2)-bird.size()); //bakkenivå
             for(int i = 0; i < (int) stage->getVector_Y().size(); i++)
             {
                 wclear(top);
                 for(int j = 0; j < bird.size(); j++){
-                        mvwaddstr(top, 27+stage->getVector_Y().at(i)+j, i+2, bird[j].c_str());
-                        mvwaddstr(top, 30, stage->getEnemyDistance()-5, "[");
-                        mvwaddstr(top, 30, stage->getEnemyDistance(), "X");
-                        mvwaddstr(top, 30, stage->getEnemyDistance()+5, "]");
+                        mvwaddstr(top, atgrass+stage->getVector_Y().at(i)+j, i+2, bird[j].c_str());
+                        mvwaddstr(top, atgrass, stage->getEnemyDistance()-5, "[");
+                        mvwaddstr(top, atgrass, stage->getEnemyDistance(), "X");
+                        mvwaddstr(top, atgrass, stage->getEnemyDistance()+5, "]");
                 }
+                printScenery();
                 wrefresh(top);
-                usleep(100000);
+                speed > 30 ? usleep(100000 * (1+((speed-30))/100)) : usleep(100000);
                 //if( i!= stage->getVector_Y().size()-1) wclear(top);
             }
 //                if(!stage->gameCheck()){
